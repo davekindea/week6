ea64b3b29ced84e6b5e4810366c3053ff8027692
davekindea <110993270+davekindea@users.noreply.github.com>
2024-10-08 22:41:41 +0300

    task4

---
+++ notebooks/cat_breaks_20241007_112332.py
@@ -0,0 +1,5 @@
+breaks_list={
+'ChannelId': ['ChannelId_1%,%ChannelId_2','ChannelId_3%,%ChannelId_5'], 
+'ProductCategory': ['airtime%,%data_bundles','financial_services%,%movies%,%other%,%ticket%,%transport%,%tv%,%utility_bill'], 
+'ProductId': ['ProductId_1%,%ProductId_10','ProductId_11%,%ProductId_12%,%ProductId_13%,%ProductId_14%,%ProductId_15%,%ProductId_16%,%ProductId_19%,%ProductId_2%,%ProductId_20%,%ProductId_21%,%ProductId_22%,%ProductId_23%,%ProductId_24%,%ProductId_27','ProductId_3%,%ProductId_4%,%ProductId_5','ProductId_6%,%ProductId_7%,%ProductId_8%,%ProductId_9'], 
+'ProviderId': ['ProviderId_1%,%ProviderId_2%,%ProviderId_3','ProviderId_4','ProviderId_5','ProviderId_6']}
